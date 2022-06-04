cfc8e3f5922e4a216a9fb66664e920bbe868fc43
Ekeru Onyeka James <safexexport01@gmail.com>
2022-06-04 03:28:01 -0700

    my first commit

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -11,11 +11,13 @@ int main(void)
 	int n;
 	int last_dig;
 
+	
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 
 	last_dig = n % 10;
 
+	
 	if (last_dig > 5)
 	{
 		printf("last digit of %d is %d and is greater than 5\n", n, last_dig);
