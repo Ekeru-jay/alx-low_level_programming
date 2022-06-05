a2df24d01bb0f80567359560a1889d956d50ce7d
Ekeru Onyeka James <safexexport01@gmail.com>
2022-06-05 07:19:12 -0700

    my first commit

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - main function  
+ *
+ * Return: end program
+ */
+int main(void)
+{
+	char low;
+	char up;
+
+	for (low = 'a'; low <= 'z'; ++low)
+		printf("%c", low);
+	for (up = 'A'; UP <= 'z'; ++UP)
+		printf("%c", UP);
+	printf("\n");
+	return (0);
+}
