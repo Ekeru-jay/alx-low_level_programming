2dce230dc7c7c10daffb543100daef5661290bc5
Ekeru Onyeka James <safexexport01@gmail.com>
2022-06-05 07:12:43 -0700

    my first commit

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 /**
- * main - main function
- *
- * Return: End the program 
- */
+* main - main function
+*
+* Return: End the program 
+*/
 int main(void)
 {char letter;
 for (letter = 'a'; letter <= 'z'; ++letter)
