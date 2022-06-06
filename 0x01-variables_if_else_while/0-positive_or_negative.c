637db7139aa9dddfcae1179bde9ba816181728ce
Ekeru Onyeka James <safexexport01@gmail.com>
2022-06-05 19:19:18 -0700

    my first commit

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -9,17 +9,17 @@
 int main(void)
 {
 	int n;
-	
+
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	
+
 	if (n > 0)
 	{
 		printf("%d is positive\n", n);
 	}
 	else if (n == 0)
 	{
-		printf("%d is zero\n", n);
+		printf("%D is zero\n", n);
 	}
 	else
 	{
@@ -27,3 +27,4 @@ int main(void)
 	}
 	return (0);
 }
+
