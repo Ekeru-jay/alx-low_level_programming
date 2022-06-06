a720985198d54610de8ae7855f0f0c6ca8d7c715
Ekeru Onyeka James <safexexport01@gmail.com>
2022-06-05 22:06:09 -0700

    my first commit

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,10 @@
 #include <stdio.h>
 /**
- * main - print a text
- *
- * Return: Always 0 (success)
+ * main - main block
+ * Return: 0
  */
 int main(void)
 {
-	puts("\"programming is like building a multilingual puzzle");
+	puts("\"programming is like building  a multilingual puzzle");
 	return (0);
 }
