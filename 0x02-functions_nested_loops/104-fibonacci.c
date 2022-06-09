3cf26067aa9e86df84552c283f17b8c59c556ecc
Ekeru Onyeka James <safexexport01@gmail.com>
2022-06-08 17:08:40 -0700

    my first commit

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -30,10 +30,10 @@ int main(void)
 				n22 = n2 % 1000000000;
 				n1 = n1 / 1000000000;
 				n2 = n2 / 1000000000;
-				boolean2 = 0
+				boolean2 = 0;
 			}
 			fn2 = (n11 + n22);
-			fn = n1 + n2 = (fn2 / 1000000000);
+			fn = n1 + n2 + (fn2 / 1000000000);
 			printf(", %ld", fn);
 			printf("%ld", fn2 % 1000000000);
 			n1 = n2;
