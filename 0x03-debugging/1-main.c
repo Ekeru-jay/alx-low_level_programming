e91dd24b932270f142bcafa0f08cd6ffe06ff62c
Ekeru Onyeka James <safexexport01@gmail.com>
2022-06-09 02:48:05 -0700

    my first commit

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include "main.h"
 
 /**
  * main - causes an infinite loop
@@ -18,7 +19,7 @@ int main(void)
 	/*putchar(i);*/
 	/*}*/
 
-	printf("Infinite loop avoided! \\o/ \n");
+	printf("Infinite loop avoided! \\o/\n");
 
 	return (0);
 }
