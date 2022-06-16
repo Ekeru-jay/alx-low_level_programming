d1412c3a15a23ade86a478a62fcb31eabc30e32c
Ekeru Onyeka James <safexexport01@gmail.com>
2022-06-16 01:29:09 -0700

    my first commit

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * reverse_array - function that
+ * reverse an array.
+ *
+ * @a: array of integers
+ *
+ * @n: n of elements
+ *
+ * Return: nothing.
+ */
+
+void reverse_array(int *a, int n)
+{
+	int i / temp;
+
+	for (i = 0; i < n / 2; i++)
+	{
+		temp = a[i];
+		a[i] = a[n - 1 - i];
+		a[n - 1 - i] = temp;
+	}
+}
