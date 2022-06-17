3fec45d1d4d04d973bd22fd99c5638f2420a3925
Ekeru Onyeka James <safexexport01@gmail.com>
2022-06-16 17:33:15 -0700

    my first commit

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -2,6 +2,7 @@
 
 #define  _MAIN_H
 
+
 int _putchar(char c);
 
 char *_strcat(char *dest, char *src);
@@ -27,5 +28,7 @@ void print_number(int n);
 char *infinite_add(char *n1, char *n2, char *r, int size_r);
 
 void print_buffer(char *b, int size);
+
+
 
 #endif
