4dcb283afe987033da42a8f5ac30736e1ea6f4e0
Ekeru Onyeka James <safexexport01@gmail.com>
2022-07-15 15:48:43 -0700

    my linked list

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -4,8 +4,8 @@
  * first - prints before main
  * Return: no return.
  */
-void __attribute__ ((constructor)) first()
+void __attribute__ ((constructor)) bmain()
 {
-	printf("you 're beat! and yet, you must allow,\n");
-	printf("I bore my house upon my bacck!\n");
+	printf("you 're beat! and yet, you must allow);
+	printf(",\nI bore my house upon my bacck!\n");
 }
